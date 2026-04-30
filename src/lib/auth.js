2048922ab7d425b90f65e7278b5347b79e542857
shawon2911 <hosenuzzaman2018@gmail.com>
2026-04-30 13:21:00 +0600

    sign up and sign in page complete

---
+++ src/lib/auth.js
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import dns from "node:dns/promises";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const client = new MongoClient(process.env.AUTH_DB_URI);
+const db = client.db();
+
+export const auth = betterAuth({
+     emailAndPassword: { 
+        enabled: true, 
+  }, 
+    database: mongodbAdapter(db, {
+        client
+    }),
+});
